b340f7d5fd16cbe15dc93af0b56042f92b2aea98
wangxinyang1983@gmail.com <359387050@qq.com>
2024-09-17 17:50:58 +0900

    feat:first commit

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,5 @@
+const LandingPageLayout = ({children}:{children:React.ReactNode}) => {
+  return <div className='flex flex-col container relative'>{children}</div>
+}
+
+export default LandingPageLayout
